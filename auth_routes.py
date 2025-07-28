612ffac56fa79227ffc7497e9193d89ad47d990b
Fabio Landin <flandin1990@gmail.com>
2025-07-28 18:03:26 -0300

    Autenticação da /login e criação da /login-form

--- auth_routes.py
+++ auth_routes.py
@@ -1,29 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
-from dependencies import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from main import bcrypt_context, ALGORITHM, ACESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordRequestForm
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 def criar_token(id_usuario, duracao_token=ACESS_TOKEN_EXPIRE_MINUTES):
-    data_expiracao = datetime.now(timezone.utc) + duracao_token
-    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    data_expiracao = datetime.now(timezone.utc) + timedelta(minutes=duracao_token)
+    dic_info = {"sub": str(id_usuario), "exp": data_expiracao}
     jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)    
     return jwt_codificado
 
-def verificar_token(token, session: Session = Depends(pegar_sessao)):
-     usuario = session.query(Usuario).filter(Usuario.id==1).first()
-     return usuario
-
-def autenticar_usuario(email, senha, session):
+def autenticar_usuario(email: str, senha: str, session: Session):
     usuario = session.query(Usuario).filter(Usuario.email == email).first()
     if not usuario:
          return False
-    elif bcrypt_context.verify(senha, usuario.senha):
+    if not bcrypt_context.verify(senha, usuario.senha):
          return False
     return usuario
 
@@ -55,12 +52,21 @@ async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sess
      if not usuario:
           raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais invalidas")
      else:
-          acess_token = criar_token(usuario.id)
-          refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
-          return {"acess_token": acess_token, "refresh_token": refresh_token, "token_type": "Bearer"}
+          access_token = criar_token(usuario.id)
+          refresh_token = criar_token(usuario.id, duracao_token=7*24*60)
+          return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+     
+#Rota de login no formulario da fastapi        
+@auth_router.post("/login-form")
+async def login_form(dados_formulario: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(pegar_sessao)):
+     usuario = autenticar_usuario(dados_formulario.username, dados_formulario.password, session)
+     if not usuario:
+          raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais invalidas")
+     else:
+          access_token = criar_token(usuario.id)
+          return {"access_token": access_token, "token_type": "bearer"}
      
 @auth_router.get("/refresh")
-async def use_refresh_token(token):
-    usuario = verificar_token()
+async def use_refresh_token(usuario: Usuario = Depends(verificar_token)):
     acess_token = criar_token(usuario.id)
     return {"acess_token": acess_token, "token_type": "Bearer"}
--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,9 @@
 from contextlib import contextmanager
-from sqlalchemy.orm import sessionmaker
-from models import db
+from main import SECRET_KEY, ALGORITHM, oauth2_schema
+from sqlalchemy.orm import sessionmaker, Session
+from models import db, Usuario
+from fastapi import Depends, HTTPException
+from jose import jwt, JWTError
 
 SessionLocal = sessionmaker(bind=db)
 
@@ -10,3 +13,14 @@ def pegar_sessao():
         yield db
     finally:
         db.close()
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usurio = int(dic_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Acesso negado, verifique a validade do token!")
+    usuario = session.query(Usuario).filter(Usuario.id==id_usurio).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso invalido!")
+    return usuario
